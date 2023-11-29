a7422254c3687b0950e357a70c823f6a09b5af89
Cameron <deancameron99@gmail.com>
2023-11-29 15:01:06 -0500

    Adding lib folder and utils file

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 interface MainNavProps {
@@ -21,7 +22,15 @@ const MainNav: React.FC<MainNavProps> = ({
        <nav 
        className="mc-6 flex items-center space-x-4 lg:space-x-6"
        >
-            main-nav
+        {routes.map(() => (
+            <Link
+            key={routes.href}
+            href={routes.href}
+            className={}
+            >
+                {routes.label}
+            </Link>
+        ))}
        </nav>
     )
 }
